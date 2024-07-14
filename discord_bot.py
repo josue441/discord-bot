80fc882b39d50c698afe51080f2f0ed3575b4490
josue <149551682+josue441@users.noreply.github.com>
2024-07-14 00:05:34 -0500

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -10,19 +10,21 @@ token = 'TOKEN' #token
 
 @bot.event
 async def on_ready():
-    channel = bot.get_channel(ID CHANNEL) #id del canal donde el bot escribira "El bot está en línea." 
+    channel = bot.get_channel(1258921236688404533)
     if channel:
-        await channel.send("El bot está en línea.")
+        await channel.send("El bot está en línea. Para la ayuda escriba $ayuda")
 
-#generador de contraseñas simple
 @bot.command(name='gen_password')
-async def gen_password(ctx):
+async def password(ctx):
     await ctx.send(f"Tu contraseña generada es: {gen_pass(15)}")
 
-#calculadora que solo suma ejemplo: $suma 10 10 el bot dira: la suma de 10 + 10 es: 20
 @bot.command(name='sumar')
 async def sumar(ctx,a,b):
     response = int(a) + int(b)
     await ctx.send(f"la suma de {a} + {b} es: {response}")
+
+@bot.command(name='ayuda')
+async def ayuda(ctx):
+    await ctx.send("comandos actuales son: $gen_password, $sumar, $repetir y $ayuda")
 
 bot.run(token)
