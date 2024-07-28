30017b60c17b396432050b9aa7d9adfcdaa1a0b5
josue <149551682+josue441@users.noreply.github.com>
2024-07-28 01:09:42 -0500

    Update bot_logic.py

--- bot_logic.py
+++ bot_logic.py
@@ -1,4 +1,4 @@
-import random
+import random, requests
 
 def gen_pass(pass_length):
     #elementos de la contraseña
@@ -10,3 +10,26 @@ def gen_pass(pass_length):
         password += random.choice(elements)
 
     return password
+
+def imagenes_de_perros():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+#funcion para solicitar imagenes de patos
+
+def get_duck():
+    url = "https://random-d.uk/api/random"
+    res = requests.get(url)
+    data = res.json()
+    return data["url"]
+
+#link de los videos
+videos = [
+        "https://www.youtube.com/watch?v=QrAta9gqYNQ&t=25s",
+        "https://www.youtube.com/watch?v=6_zxPQuK1PY",
+        "https://www.youtube.com/watch?v=uBMW8QM_iSI"
+]
+#funcion  que elige 1 de los 3 videos
+def videos_choice():
+    return random.choice(videos)
